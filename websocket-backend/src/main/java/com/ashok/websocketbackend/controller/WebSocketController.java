6f1fbc957c66a7f57be88d72aaec2841cf23ac8d
asvyas <asvyas@L10INMUM01D2507.asi.bvcorp.corp>
2020-03-11 13:36:03 +0530

    log implementation.....

--- websocket-backend/src/main/java/com/ashok/websocketbackend/controller/WebSocketController.java
+++ websocket-backend/src/main/java/com/ashok/websocketbackend/controller/WebSocketController.java
@@ -2,6 +2,8 @@ package com.ashok.websocketbackend.controller;
 
 import java.text.SimpleDateFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -10,11 +12,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
+	
 	@Autowired
 	private SimpMessagingTemplate template;
 	
 	@MessageMapping("/send/message") 
 	public void onRecieveMessage(String message){
+		
+		logger.debug(message);
+		
 		template.convertAndSend("/chat", message);
 	}
 
